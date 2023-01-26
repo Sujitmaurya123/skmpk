59890e5fff832d5a0c3bb160339088a1531621ec
Sujitmaurya123 <sujitkic6802maurya@gmail.com>
2023-01-26 11:09:12 +0530

    sujitbhai

---
+++ list in py1.py
@@ -0,0 +1,12 @@
+#marks=[3,4,6,"Sujit","kumar",3,5]
+#print(marks[2])
+#print(marks[-2])#total length (5-2=3)
+#print(marks[0:7:2])
+'''l1=[1,2,5,3,6,4]
+print(l1)
+l1.sort()
+print("sorting list is :")
+print(l1)'''
+af=[2,4,6,7,8]
+af.reverse()
+print(af)
